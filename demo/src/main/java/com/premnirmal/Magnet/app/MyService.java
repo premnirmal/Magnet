ba0168721349c32520152ad813978efb26d1e96f
Prem Nirmal <prem@fuzzproductions.com>
2014-07-21 10:44:47 -0400

    removed line from service

--- demo/src/main/java/com/premnirmal/Magnet/app/MyService.java
+++ demo/src/main/java/com/premnirmal/Magnet/app/MyService.java
@@ -30,7 +30,6 @@ public class MyService extends MagnetService {
     protected Notification createNotification() {
         Intent notificationIntent = new Intent(this, ParanormalActivity.class);
         Notification notification;
-        Build.VERSION_CODES.CUPCAKE
         if (Build.VERSION.SDK_INT < Build.VERSION_CODES.HONEYCOMB) {
             notification = new Notification(R.drawable.ic_launcher, "Hello", System.currentTimeMillis());
             notification.contentIntent = PendingIntent.getActivity(this, 0, notificationIntent, 0);
